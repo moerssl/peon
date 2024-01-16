7cdc9af580e88d78e7312f2fd8b58f601028627c
moerssl <ci@agilevolunteer.com>
2024-01-16 12:52:54 +0100

    - add char with enter - show chars without rating - change order of boxes - persist order

--- pages/dungeons.vue
+++ pages/dungeons.vue
@@ -1,47 +1,62 @@
 <template>
     <v-row>
       <v-col>
-        <v-text-field label="realm" v-model="realm"></v-text-field>
+        <v-text-field label="Realm" v-model="realm" @keydown.enter="add"></v-text-field>
       </v-col>
       <v-col>
-        <v-text-field label="name" v-model="name"></v-text-field>
+        <v-text-field label="Name" v-model="name" @keydown.enter="add"></v-text-field>
       </v-col>
       <v-col  class="mt-2">
         <v-btn @click="add">Hinzufügen</v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="6" lg="2" v-for="(value, key) in dungeons">
-        <v-card class="pa-2">
-          <v-card-title>
-            {{ value.name }} ({{ value.mythic_plus_scores_by_season[0].scores.all }})
-            
-          </v-card-title>
-          <v-card-text>
-            <table>
-              <tr v-for="run in value.mythic_plus_weekly_highest_level_runs">
-                <td>
-                  {{ run.short_name }}
-                </td>
-                <td class="text-right">
-                  {{ run.mythic_level }}
-                </td>
-                <td>
-                  +{{ run.num_keystone_upgrades }}
-                </td>
-              </tr>
-            </table>
-            <v-card-actions class="float-right">
-              <v-btn icon="mdi-trash-can-outline" variant="plain" @click="remove(value, key)"></v-btn>
-            </v-card-actions>
-          </v-card-text>
-        </v-card>
+ 
+      <draggableComponent
+      v-model="chars" 
+      group="people" 
 
-          
-      </v-col>
-    </v-row>
+      item-key="name"
+      class="v-row"
+      @end="save"
+      >
+      <template #item="{element}">
+        <v-col cols="6" lg="2" ><!-- /v-col>v-for="value in chars" -->
+          <v-card class="pa-2">
+            <v-card-title>
+  
+              {{ element.name }} <span v-if="element?.runs">({{ element.runs.mythic_plus_scores_by_season[0].scores.all }})</span>
+              
+            </v-card-title>
+            <v-card-text v-if="element.runs">
+              <table>
+                <tr v-for="run in element.runs.mythic_plus_weekly_highest_level_runs">
+                  <td>
+                    {{ run.short_name }}
+                  </td>
+                  <td class="text-right">
+                    {{ run.mythic_level }}
+                  </td>
+                  <td>
+                    +{{ run.num_keystone_upgrades }}
+                  </td>
+                </tr>
+              </table>
+  
+            </v-card-text>
+            <v-card-actions class="float-right">
+              <v-btn icon="mdi-trash-can-outline" variant="plain" @click="remove(element)"></v-btn>
+            </v-card-actions>
+          </v-card>
+
+            
+        </v-col>
+      </template>
+    </draggableComponent>
+   
 </template>
 <script setup>
+import draggableComponent from 'vuedraggable';
+
 const STORAGE_KEY ="dungeon-chars"
 const realm = ref()
 const name = ref()
@@ -52,6 +67,8 @@ const loadRuns = async char => {
   const slug = `${char.realm}-${char.name}`;
   const data = await $fetch(`/api/dungeons?name=${char.name}&realm=${char.realm}`)
   dungeons.value[slug] = data
+  char["runs"] = data
+  save()
 }
 
 
@@ -76,6 +93,11 @@ const add = () => {
   loadRuns(char)
 
   setItem(STORAGE_KEY, chars.value)
+}
+
+const save = () => {
+  setItem(STORAGE_KEY, chars.value)
+
 }
 
 const remove = (runToRemove, slugToRemove) => {
@@ -113,7 +135,7 @@ const remove = (runToRemove, slugToRemove) => {
 
   console.log(index)
   chars.value.splice(index,1)
-  setItem(STORAGE_KEY, chars.value)
+  save()
 }
 
 function getItem(item) {
@@ -130,7 +152,17 @@ function setItem(item, value) {
   
     return true
   } else {
-    return false
-  }
+    return false  } 
 }
 </script>
+<style scoped lang="scss">
+.v-card {
+  cursor: grab;
+  &-title {
+    text-transform: capitalize;
+  }
+  .sortable-chosen {
+    cursor: grabbing;
+  }
+}
+</style>
