dee4c3839447542605c7f627fd99af45af3f36fb
moerssl <ci@agilevolunteer.com>
2024-02-12 11:16:58 +0100

    logging

--- server/api/tm/mats.ts
+++ server/api/tm/mats.ts
@@ -5,11 +5,17 @@ import { AhImporter } from '~/utils/AhImporter';
 export default defineEventHandler(async (event) => { 
   const tsm = useTsm();
   const ah = new AhImporter();
-
+  console.info("Accepted request")
   const resultPromise = tsm.getCommodities(2);
-  const importPromise = resultPromise.then(result => ah.importData(result));
+
+  const importPromise = resultPromise.then(result => {
+  console.info("Received commodities")
+    
+    ah.importData(result)
+  });
+  console.info("set status code")
 
   //await Promise.all([resultPromise, importPromise]);
   setResponseStatus(event,202, 'Accepted')
-  return; // HTTP Code accepted
+  return "this is fine"; // HTTP Code accepted
 });
