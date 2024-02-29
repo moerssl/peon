7799a8b8f40a567e844eb2fecf29adc9661183e0
moerssl <ci@agilevolunteer.com>
2024-02-29 15:26:02 +0100

    polling

--- pages/admin/se.vue
+++ pages/admin/se.vue
@@ -1,9 +1,20 @@
 <template>
   <v-row>
+    <Head>
+      <Title v-if="countdownTimer">
+        {{ countdownTimer }} - {{ ahMonitor }}
+      </Title>
+      <Title>
+        {{ ahMonitor }}
+      </Title>
+    </Head>
     <v-col>
+      <v-btn @click="loadShopping">Aktualisieren</v-btn>
+      <v-btn @click="startPollingWithRandomInterval">Auto Reload</v-btn>
       <v-btn @click="copyList">Kopieren</v-btn>
       {{ Kopiert }}
     </v-col>
+    <v-col>{{ countdownTimer }}</v-col>
   </v-row>
   <v-row>
       <v-col v-for="(item, index) in shoppinglist" :key="index" cols="12" lg="3">
@@ -48,13 +59,23 @@
 </template>
 <script setup>
   const shoppinglist = ref()
-  const loadShopping = () => {
-    $fetch('/api/se').then(data => {
-      shoppinglist.value = data
-    })
+  const loadShopping = async () => {
+    const result = await $fetch('/api/se')
+    shoppinglist.value = result
   }
 
+  
+  const timeRemaining = ref(null);
   const Kopiert = ref()
+  const previousData = ref(null);
+  const countdownTimer = ref(null)
+  const ahMonitor = ref("Peon AH Monitor")
+  const newDataAvailable =ref(false)
+
+  let intervalId = null;
+  let countdownIntervalId = null;
+
+
 
   const intoShoppingList = computed(() => {
     if (shoppinglist.value == undefined) return "";
@@ -96,9 +117,103 @@
       Kopiert.value = "Fehler :("
 
     });
+    newDataAvailable.value = false
   }
+
+
+
+  const startPollingWithRandomInterval = () => {
+    const randomInterval = getRandomInterval();
+    clearInterval(countdownIntervalId);
+    timeRemaining.value = randomInterval / 1000
+    intervalId = setTimeout(monitor, randomInterval);
+    startTicking();
+  };
+
+  const stopPolling = () => {
+    clearInterval(intervalId);
+  };
+
+  const getRandomInterval = () => {
+    // Generate a random interval between 15 seconds and 15 minutes (900000 milliseconds)
+    return Math.floor(Math.random() * (900000 - 15000) + 15000);
+  };
+  const monitor = async () => {
+    try {
+      await loadShopping()
+      const newData = shoppinglist.value
+      if (!previousData.value) {
+        previousData.value = newData;
+        return
+      }
+
+      // Compare newData with previousData to detect changes
+      if (JSON.stringify(newData) !== JSON.stringify(previousData.value)) {
+        // Trigger browser notification
+        showBrowserNotification('New data available!');
+        newDataAvailable.value = true
+      }
+
+      previousData.value = newData;
+      startPollingWithRandomInterval(); 
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      startPollingWithRandomInterval(); 
+    }
+  };
+
+  const startTicking = () => {
+    // Update timeRemaining every second
+    countdownIntervalId = setInterval(() => {
+      timeRemaining.value -= 1;
+      countdownTimer.value = formatTime(timeRemaining.value)
+
+      if (newDataAvailable.value == true) {
+        if ((Math.floor(timeRemaining.value) % 4) == 0) {
+          ahMonitor.value = ""
+        } else {
+          ahMonitor.value = "Neue Daten!"
+        }
+      } else {
+        ahMonitor.value = "Peon AH Monitor"
+      }
+
+
+      
+      if (timeRemaining.value <= 1.5) {
+        clearInterval(countdownIntervalId);
+      }
+    }, 1000);
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
+  const showBrowserNotification = (message) => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support system notifications");
+    } else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      new Notification(message);
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function(permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          new Notification(message);
+        }
+      });
+    }
+  };
 
   onMounted(() => {
     loadShopping();
   })
+
+  onBeforeUnmount(() => {
+    // Clean up resources when the component is unmounted
+    stopPolling();
+  });
 </script>
